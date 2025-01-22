2a01bef14188ba8dd314283eeb77a6b764bab0fc
petras <petrasvestartas@gmail.com>
2025-01-22 14:41:30 +0100

    Bump version to 0.3.0

--- src/compas_grid/__init__.py
+++ src/compas_grid/__init__.py
@@ -13,7 +13,7 @@ __author__ = ["Petras Vestartas"]
 __copyright__ = "Petras Vestartas"
 __license__ = "MIT License"
 __email__ = "petrasvestartas@gmail.com"
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 HERE = os.path.dirname(__file__)
 
